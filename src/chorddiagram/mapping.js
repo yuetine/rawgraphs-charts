54a99e74f4528e1538f38c68e0e1c155083fcde9
yuetine <155622891+yuetine@users.noreply.github.com>
2025-02-03 22:11:46 +0100

    Adjust node sorting for grouping in chord diagram.

--- src/chorddiagram/mapping.js
+++ src/chorddiagram/mapping.js
@@ -16,6 +16,16 @@ export const mapData = function (data, mapping, dataTypes, dimensions) {
 
   const results = []
 
+// 手动调整分组顺序
+data.sort((a, b) => {
+    const order = ["1NS", "2AS", "3MS", "A WALKING", "B CYCLING", "C LEI"];
+    return (
+        order.indexOf(a[mapping.source.value]) - order.indexOf(b[mapping.source.value])
+    );
+});
+
+
+  
   const result = d3.rollups(
     data,
     (v) => {
